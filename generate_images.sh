5e3d348568c8cfda342fb62fbc30498e169915a0
Denis Chaimow <dchaimow@users.noreply.github.com>
2021-01-05 18:45:50 +0100

    update generate_docker function, base, pkg included

--- generate_images.sh
+++ generate_images.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 # This is the neurodocker script.
+
+set -e
+
+image=repronim/neurodocker:0.7.0
+
+generate_docker() {
+    docker run --rm ${image} \
+           generate docker \
+           --base debian:stretch \
+           --pkg-manager apt       
+}
+
+generate_docker > Dockerfile
